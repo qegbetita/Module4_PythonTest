34891a5e67b23ebd5a23811483dc52a454bee356
qegbetita <qegbetita@tip.edu.ph>
2023-10-03 08:04:07 +0000

    Module 4 Activity

---
+++ test_check.py
@@ -0,0 +1,15 @@
+import unittest
+
+def check(x):
+
+    return x>=100
+
+class myTest(unittest.TestCase):
+
+    def test(self):
+
+        self.assertTrue(check(100))
+
+if __name__ == '__main__':
+
+    unittest.main()
